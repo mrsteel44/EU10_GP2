59051aa0913aa09787d7393f8b3d24c263d84e91
Aysegul Soytas Celik <aysegulsoytaszb@gmail.com>
2023-04-06 22:57:56 +0100

    Aysegul's first commit

--- src/test/java/Aysegul/Aysegul.java
+++ src/test/java/Aysegul/Aysegul.java
@@ -3,5 +3,6 @@ package Aysegul;
 public class Aysegul {
     public static void main(String[] args) {
         System.out.println("This is Ayseguls's first commit");
+        System.out.println("Why it didn't work? :)");
     }
 }
